7f180035634c9e1db02edc39c76931ea572d9e73
Mathieu GHENNASSIA <mathieu.ghennassia@gmail.com>
2014-04-29 19:06:44 -0700

    Add model User

---
+++ Hot Spot/Hot Spot/Classes/User.h
@@ -0,0 +1,33 @@
+//
+//  User.h
+//  Hot Spot
+//
+//  Created by Mathieu GHENNASSIA on 29/04/2014.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic) NSString *userId;
+@property (nonatomic) NSString *sessionToken;
+
+typedef void (^UserSuccessBlock)(id responseObject);
+typedef void (^UserFailureBlock)(NSInteger statusCode, NSError *error, id responseObject);
+
++ (User *)CurrentUser;
+- (void)signup:(NSString *)email
+      password:(NSString *)password
+       success:(UserSuccessBlock)success
+       failure:(UserFailureBlock)failure;
+- (void)login:(NSString *)userId
+ sessionToken:(NSString *)sessionToken
+      success:(UserSuccessBlock)success
+      failure:(UserFailureBlock)failure;
+- (void)login:(NSString *)email
+     password:(NSString *)password
+      success:(UserSuccessBlock)success
+      failure:(UserFailureBlock)failure;
+
+@end
---
+++ Hot Spot/Hot Spot/Classes/User.m
@@ -0,0 +1,92 @@
+//
+//  User.m
+//  Hot Spot
+//
+//  Created by Mathieu GHENNASSIA on 29/04/2014.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+#import "User.h"
+
+#import <AFNetworking/AFNetworking.h>
+#import "API.h"
+
+@implementation User
+
++ (User *)CurrentUser {
+    static User *_sharedInstance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedInstance = [[User alloc] init];
+    });
+
+    return _sharedInstance;
+}
+
+- (void)initWithResponseObject:(NSDictionary *)responseObject {
+    if (responseObject[@"userId"]) {
+        self.userId = responseObject[@"userId"];
+    }
+    if (responseObject[@"sessionToken"]) {
+        self.sessionToken = responseObject[@"sessionToken"];
+    }
+}
+
+- (void)signup:(NSString *)email
+      password:(NSString *)password
+       success:(UserSuccessBlock)success
+       failure:(UserFailureBlock)failure {
+    NSDictionary *parameters = @{@"email":email, @"password":password};
+
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:[API signupUrl]
+      parameters:parameters
+         success:^(AFHTTPRequestOperation *operation, id responseObject) {
+             [self initWithResponseObject:responseObject];
+             success(responseObject);
+         }
+         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             [self initWithResponseObject:operation.responseObject];
+             failure(operation.response.statusCode, error, operation.responseObject);
+         }];
+}
+
+- (void)login:(NSString *)userId
+ sessionToken:(NSString *)sessionToken
+      success:(UserSuccessBlock)success
+      failure:(UserFailureBlock)failure {
+    NSDictionary *parameters = @{@"userId":userId, @"sessionToken":sessionToken};
+
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:[API loginUrl]
+      parameters:parameters
+         success:^(AFHTTPRequestOperation *operation, id responseObject) {
+             [self initWithResponseObject:responseObject];
+             success(responseObject);
+         }
+         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             [self initWithResponseObject:operation.responseObject];
+             failure(operation.response.statusCode, error, operation.responseObject);
+         }];
+}
+
+- (void)login:(NSString *)email
+     password:(NSString *)password
+      success:(UserSuccessBlock)success
+      failure:(UserFailureBlock)failure {
+    NSDictionary *parameters = @{@"email":email, @"password":password};
+
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:[API loginUrl]
+      parameters:parameters
+         success:^(AFHTTPRequestOperation *operation, id responseObject) {
+             [self initWithResponseObject:responseObject];
+             success(responseObject);
+         }
+         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             [self initWithResponseObject:operation.responseObject];
+             failure(operation.response.statusCode, error, operation.responseObject);
+         }];
+}
+
+@end
