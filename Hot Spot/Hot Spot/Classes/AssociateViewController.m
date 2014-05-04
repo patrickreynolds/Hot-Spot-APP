24fda5e4d86d7ace67cac8e4f5d943ed1b567944
Mathieu GHENNASSIA <mathieu.ghennassia@gmail.com>
2014-05-03 17:02:00 -0700

    Implement view Associate

--- Hot Spot/Hot Spot/Classes/AssociateViewController.m
+++ Hot Spot/Hot Spot/Classes/AssociateViewController.m
@@ -8,6 +8,34 @@
 
 #import "AssociateViewController.h"
 
+#import "Instagram.h"
+
 @implementation AssociateViewController
+
+- (IBAction)didTapConnect:(UIButton *)sender {
+    if (!sender.isEnabled) {
+        return ;
+    }
+    sender.enabled = NO;
+    [[InstagramPlatform sharedPlatform] startSessionWithClientID:@"801cf1d44fcd43f99cbb38781d7cb227"
+                                                    clientSecret:@"f768ff10426f4cc2a6a8e6688b45a39f"
+                                                       authScope:InstagramPlatformAuthScopeAll
+                                                      completion:^(NSString *accessToken, NSError *error) {
+                                                          sender.enabled = YES;
+                                                          if (error == nil) {
+                                                              [[[UIAlertView alloc] initWithTitle:@"Success"
+                                                                                          message:[NSString stringWithFormat:@"Instagram access_token: %@", accessToken]
+                                                                                         delegate:nil
+                                                                                cancelButtonTitle:@"OK"
+                                                                                otherButtonTitles:nil] show];
+                                                          } else {
+                                                              [[[UIAlertView alloc] initWithTitle:@"Error"
+                                                                                          message:error.localizedDescription
+                                                                                         delegate:nil
+                                                                                cancelButtonTitle:@"OK"
+                                                                                otherButtonTitles:nil] show];
+                                                          }
+                                                      }];
+}
 
 @end
