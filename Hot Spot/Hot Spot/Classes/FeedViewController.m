b530374bf924b65d179cb49f1677b61c273ef28c
Mathieu GHENNASSIA <mathieu.ghennassia@gmail.com>
2014-05-06 15:40:59 -0700

    Create classes FeedViewController, SearchViewController, and HotSpotsTableViewController

---
+++ Hot Spot/Hot Spot/Classes/FeedViewController.m
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.m
+//  Hot Spot
+//
+//  Created by Mathieu GHENNASSIA on 06/05/2014.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+#import "FeedViewController.h"
+
+@interface FeedViewController ()
+
+@end
+
+@implementation FeedViewController
+
+@end
---
+++ Hot Spot/Hot Spot/Classes/SearchViewController.m
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.m
+//  Hot Spot
+//
+//  Created by Mathieu GHENNASSIA on 06/05/2014.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+@end
