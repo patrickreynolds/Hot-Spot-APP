9da370485b2ab5c4915bd0d2737e2d484838a34a
Mathieu GHENNASSIA <mathieu.ghennassia@gmail.com>
2014-05-06 18:43:25 -0700

    LocalizedMediaStreamViewController: Implement the required methods of protocol UICollectionViewDataSource using the delegate LocalizedMediaStreamDelegate

--- Hot Spot/Hot Spot/Classes/LocalizedMediaStreamViewController.m
+++ Hot Spot/Hot Spot/Classes/LocalizedMediaStreamViewController.m
@@ -8,10 +8,51 @@
 
 #import "LocalizedMediaStreamViewController.h"
 
+#import "LocalizedMediaStreamCell.h"
+#import "LocalizedMedia.h"
+
 @interface LocalizedMediaStreamViewController ()
 
 @end
 
 @implementation LocalizedMediaStreamViewController
+
+#pragma mark - UIViewController
+
+/*
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ [segue destinationViewController]
+ }
+ */
+
+#pragma mark - UICollectionViewController
+
+#pragma mark - UICollectionViewDelegate
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(numberOfLocalizedMedia)]) {
+        return [self.delegate numberOfLocalizedMedia];
+    } else {
+        [NSException raise:NSInternalInconsistencyException format:@"Attribute delegate not set"];
+        return 0;
+    }
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    LocalizedMediaStreamCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"LocalizedMediaStreamCell" forIndexPath:indexPath];
+    LocalizedMedia *localizedMedia;
+
+    if (self.delegate && [self.delegate respondsToSelector:@selector(numberOfLocalizedMedia)]) {
+        localizedMedia = [self.delegate localizedMediaForRow:indexPath.row];
+    } else {
+        [NSException raise:NSInternalInconsistencyException format:@"Attribute delegate not set"];
+    }
+
+    cell.username.text = localizedMedia.username;
+
+    return cell;
+}
 
 @end
