b4614320ee4e170d5f730f3bd387d9b44d1d904e
Mathieu GHENNASSIA <mathieu.ghennassia@gmail.com>
2014-05-06 18:32:49 -0700

    Create protocol LocalizedMediaStreamDelegate

--- Hot Spot/Hot Spot/Classes/LocalizedMediaStreamViewController.h
+++ Hot Spot/Hot Spot/Classes/LocalizedMediaStreamViewController.h
@@ -6,6 +6,18 @@
 //  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
 //
 
-@interface LocalizedMediaStreamViewController : UICollectionViewController
+@class LocalizedMedia;
+
+@protocol LocalizedMediaStreamDelegate <NSObject>
+
+@required
+- (NSInteger)numberOfLocalizedMedia;
+- (LocalizedMedia *)localizedMediaForRow:(NSInteger)row;
+
+@end
+
+@interface LocalizedMediaStreamViewController : UICollectionViewController <UICollectionViewDelegate, UICollectionViewDataSource>
+
+@property (weak, nonatomic) id<LocalizedMediaStreamDelegate> delegate;
 
 @end
