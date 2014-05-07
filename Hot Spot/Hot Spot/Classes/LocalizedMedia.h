fbd4c842e342891ea6fc6523f7c374f00fc1357a
Mathieu GHENNASSIA <mathieu.ghennassia@gmail.com>
2014-05-06 17:44:32 -0700

    Create empty model LocalizedMedia

---
+++ Hot Spot/Hot Spot/Classes/LocalizedMedia.h
@@ -0,0 +1,11 @@
+//
+//  LocalizedMedia.h
+//  Hot Spot
+//
+//  Created by Mathieu GHENNASSIA on 06/05/2014.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+@interface LocalizedMedia : NSObject
+
+@end
---
+++ Hot Spot/Hot Spot/Classes/LocalizedMedia.m
@@ -0,0 +1,13 @@
+//
+//  LocalizedMedia.m
+//  Hot Spot
+//
+//  Created by Mathieu GHENNASSIA on 06/05/2014.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+#import "LocalizedMedia.h"
+
+@implementation LocalizedMedia
+
+@end
