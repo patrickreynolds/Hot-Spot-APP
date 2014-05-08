9e0d458cb4d2d11790a86de2750fab697f6e755e
Mathieu GHENNASSIA <mathieu.ghennassia@gmail.com>
2014-05-07 22:34:10 -0700

    Create model HotSpot

---
+++ Hot Spot/Hot Spot/Classes/HotSpot.h
@@ -0,0 +1,15 @@
+//
+//  HotSpot.h
+//  Hot Spot
+//
+//  Created by Mathieu GHENNASSIA on 07/05/2014.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+@interface HotSpot : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSNumber *longitude;
+@property (strong, nonatomic) NSNumber *latitude;
+
+@end
---
+++ Hot Spot/Hot Spot/Classes/HotSpot.m
@@ -0,0 +1,13 @@
+//
+//  HotSpot.m
+//  Hot Spot
+//
+//  Created by Mathieu GHENNASSIA on 07/05/2014.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+#import "HotSpot.h"
+
+@implementation HotSpot
+
+@end
